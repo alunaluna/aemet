6f253c509d7303157634e49d11f72de0f0257fad
Álvaro  Luna <alvaro.lunaluna@uma.es>
2021-01-17 18:11:50 +0100

    Rompiendo Heroku def

--- app/Http/Controllers/AemetHelper.php
+++ app/Http/Controllers/AemetHelper.php
@@ -14,6 +14,8 @@ class AemetHelper
 	function __construct()
 	{
 
+	    //FORMA FÁCIL QUE NO FUNCIONA
+
         //$response = Http::withHeaders([ //Meter la api key de aemet en el .ENV
         //    'api_key' => 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJncmFmZml0ZXJvbWFzQGdtYWlsLmNvbSIsImp0aSI6ImVkMThiMDM1LTdmYTgtNGQxYy1hNmY2LWQ4NDQ1Y2ZmZDM4ZCIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjA4NTcxNjE1LCJ1c2VySWQiOiJlZDE4YjAzNS03ZmE4LTRkMWMtYTZmNi1kODQ0NWNmZmQzOGQiLCJyb2xlIjoiIn0.US6H11IPALR5cqM5SsAK4Yc3XqblEv4t5RvkAFpRhA4'
         //])->get('https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/29067');
@@ -22,46 +24,51 @@ class AemetHelper
 
         //$response_tiempo = Http::get($url_tiempo);
 
-        ///////$response_tiempo = Http::get('https://opendata.aemet.es/opendata/sh/6be0a230');
-
         //$tiempo = json_decode($response_tiempo->getBody(),true);
 
         //dd($tiempo);
 
         //cURL !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-        $ch = curl_init();
+        $ch1 = curl_init();
         $headers = array(
             'api_key: eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJncmFmZml0ZXJvbWFzQGdtYWlsLmNvbSIsImp0aSI6ImVkMThiMDM1LTdmYTgtNGQxYy1hNmY2LWQ4NDQ1Y2ZmZDM4ZCIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjA4NTcxNjE1LCJ1c2VySWQiOiJlZDE4YjAzNS03ZmE4LTRkMWMtYTZmNi1kODQ0NWNmZmQzOGQiLCJyb2xlIjoiIn0.US6H11IPALR5cqM5SsAK4Yc3XqblEv4t5RvkAFpRhA4'
         );
-        curl_setopt($ch, CURLOPT_URL, 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/29067');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_SSL_CIPHER_LIST, 'DEFAULT@SECLEVEL=1');
+        curl_setopt($ch1, CURLOPT_URL, 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/29067');
+        curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch1, CURLOPT_HEADER, 0);
+        curl_setopt($ch1, CURLOPT_SSL_CIPHER_LIST, 'DEFAULT@SECLEVEL=1');
+        curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
 
         // execute
-        $output = curl_exec($ch);
+        //$output = curl_exec($ch1);
+
+        $url_tiempo = json_decode(curl_exec($ch1),true)['datos'];
 
         // free
-        curl_close($ch);
+        curl_close($ch1);
 
 
-        $ch = curl_init();
+        $ch2 = curl_init();
         $headers = array(
             'api_key: eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJncmFmZml0ZXJvbWFzQGdtYWlsLmNvbSIsImp0aSI6ImVkMThiMDM1LTdmYTgtNGQxYy1hNmY2LWQ4NDQ1Y2ZmZDM4ZCIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjA4NTcxNjE1LCJ1c2VySWQiOiJlZDE4YjAzNS03ZmE4LTRkMWMtYTZmNi1kODQ0NWNmZmQzOGQiLCJyb2xlIjoiIn0.US6H11IPALR5cqM5SsAK4Yc3XqblEv4t5RvkAFpRhA4'
         );
-        curl_setopt($ch, CURLOPT_URL, 'https://opendata.aemet.es/opendata/sh/6be0a230');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_SSL_CIPHER_LIST, 'DEFAULT@SECLEVEL=1');
+        curl_setopt($ch2, CURLOPT_URL, $url_tiempo);
+        curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch2, CURLOPT_HEADER, 0);
+        curl_setopt($ch2, CURLOPT_SSL_CIPHER_LIST, 'DEFAULT@SECLEVEL=1');
+        curl_setopt($ch2,CURLOPT_RETURNTRANSFER,1);
 
         // execute
-        $output = curl_exec($ch);
+        //$output = curl_exec($ch2);
+
+        $tiempo = json_decode(curl_exec($ch2),true);
+        dd($tiempo);
 
         // free
-        curl_close($ch);
+        curl_close($ch2);
 
-        dd($output);
+        //dd($output);
 
 
 	}
